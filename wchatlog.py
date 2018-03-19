00ce5fbe2750897f40c7b8131e8080ca5213fdb7
smith <smithemail@163.com>
2018-03-19 17:54:46 +0800

    optimization, 优化文件存储，可按日期分文件夹保存

--- wchatlog.py
+++ wchatlog.py
@@ -12,38 +12,48 @@ import datetime
 import threading
 import wxpy
 
+from optparse import OptionParser
 from wxpy import TEXT, PICTURE, SHARING, RECORDING
 
 
 class DownloadTask(object):
     '''下载任务单元， msg为register中的msg'''
-    def __init__(self, msg, save_text):
+    def __init__(self, msg, file_type, date_str, save_name):
         self.msg = msg
-        self.save_text = save_text
+        self.file_type = file_type
+        self.date_str = date_str
+        self.save_name = save_name
 
 
 class Downloader(threading.Thread):
-    def __init__(self, download_queue):
+    def __init__(self, download_queue, save_dir, dir_level):
         self.download_queue = download_queue
+        self.save_directory = save_dir
+        self.dir_level = dir_level
         super(Downloader, self).__init__()
-
-    def _save_dir(self, string):
-        return string.split('/')[0]
 
     def _remove_empty_file(self, path):
         if os.path.exists(path) and not os.path.getsize(path):
             os.remove(path)
 
+    def _prefix_dir(self, file_type, date_str):
+        date = datetime.datetime.strptime(date_str, "%Y%m%d")
+        pfdir = self.save_directory + '/' + file_type + '/'
+        if self.dir_level == "year":
+            pfdir += str(date.year)
+        elif self.dir_level == "month":
+            pfdir += str(date.year) + '/' + str(date.month)
+        elif self.dir_level == "day":
+            pfdir += str(date.year) + '/' + str(date.month) + '/' + str(date.day)
+        else:
+            pfdir += str(date.year)
+        os.makedirs(pfdir, exist_ok=True)
+        return pfdir
+
     def download(self, task):
-        year = datetime.datetime.now().strftime("%Y")
-        # save_text必须前缀保存文件夹。eg: image_dir/test_image.png
-        save_dir = self._save_dir(task.save_text)
-        if not os.path.isdir(year):
-            os.mkdir(year)
-        if not os.path.isdir(year + '/' + save_dir):
-            os.mkdir(year + '/' + save_dir)
-        task.msg.get_file(year + '/' + task.save_text)
-        self._remove_empty_file(year + '/' + task.save_text)
+        prefix_dir = self._prefix_dir(task.file_type, task.date_str)
+        task.msg.get_file(prefix_dir + '/' + task.save_name)
+        self._remove_empty_file(prefix_dir + '/' + task.save_name)
 
     def run(self):
         while True:
@@ -64,16 +74,18 @@ class MessageLog(object):
         return self.ctime > other.ctime
 
 
-class Writer(threading.Thread):
-    def __init__(self, msg_queue):
+class MessageWriter(threading.Thread):
+    def __init__(self, msg_queue, save_dir):
         self.msg_queue = msg_queue
-        super(Writer, self).__init__()
+        self.save_directory = save_dir
+        super(MessageWriter, self).__init__()
+
+    def _prefix_dir(self):
+        os.makedirs(self.save_directory, exist_ok=True)
+        return self.save_directory
 
     def write_to_file(self, chat_name, group_mber, sname, ctime, message):
-        year = datetime.datetime.now().strftime("%Y")
-        if not os.path.isdir(year):
-            os.mkdir(year)
-        friend_file = year + '/' + chat_name
+        friend_file = self._prefix_dir() + '/' + chat_name
         with open(friend_file, 'a+') as fp:
             fp.write('[' + ctime.strftime("%Y-%m-%d %H:%M:%S") + ']')
             if group_mber:
@@ -117,25 +129,26 @@ class ChatLog(object):
             chat = msg.chat
             sender = msg.sender
             ctime = self._cn_datetime(msg.raw.get('CreateTime'))
+            date_str = ctime.strftime("%Y%m%d")
             group_member = msg.member.name if isinstance(chat, wxpy.Group) else ''
             sender_name = group_member if group_member else sender.name
             message_text = msg.text
 
             if msg.type == PICTURE:
-                message_text = ctime.strftime("%Y%m%d%H%M%S_") + chat.name + '_' + sender_name
+                message_text = ctime.strftime("%Y%m%d%H%M%S_")\
+                               + chat.name + '_' + sender_name
                 message_text += '_' + msg.raw['FileName'].replace('.png', '.jpeg')
-                message_text = 'images/' + message_text
-                download_task = DownloadTask(msg, message_text)
+                download_task = DownloadTask(msg, 'images', date_str, message_text)
                 self.download_queue.put(download_task)
-                message_text = '![](' + message_text + ')'
+                message_text = '![](images/' + message_text + ')'
 
             if msg.type == RECORDING:
-                message_text = ctime.strftime("%Y%m%d%H%M%S_") + chat.name + '_' + sender_name
+                message_text = ctime.strftime("%Y%m%d%H%M%S_")\
+                               + chat.name + '_' + sender_name
                 message_text += '_' + msg.raw['FileName']
-                message_text = 'recordings/' + message_text
-                download_task = DownloadTask(msg, message_text)
+                download_task = DownloadTask(msg, 'recordings', date_str, message_text)
                 self.download_queue.put(download_task)
-                message_text = '![](' + message_text + ')'
+                message_text = '![](recordings/' + message_text + ')'
 
             msg_log = MessageLog(chat.name, group_member,
                                  sender.name, ctime, message_text)
@@ -144,13 +157,23 @@ class ChatLog(object):
         self.bot.join()
 
 
+def option_parser():
+    parser = OptionParser()
+    parser.add_option("-l", "--level", type="string", dest="dir_level", default="year",
+                      help="文件夹细分粒度，year,month,day, 只对下载文件有效\
+                            (the deep dir to save file, only valid for download file)")
+    parser.add_option("-s", "--save", type="string", dest="save_dir", default="data",
+                      help="保存日志的路径(directory where store logs)")
+    return parser.parse_args()
+
 if __name__ == '__main__':
+    options, args = option_parser()
     msg_queue = queue.PriorityQueue(maxsize=1000)
     download_queue = queue.Queue(1000)
 
-    writer = Writer(msg_queue)
-    writer.start()
-    downloader = Downloader(download_queue)
+    messagewriter = MessageWriter(msg_queue, options.save_dir)
+    messagewriter.start()
+    downloader = Downloader(download_queue, options.save_dir, options.dir_level)
     downloader.start()
 
     chatlog = ChatLog(msg_queue, download_queue)
